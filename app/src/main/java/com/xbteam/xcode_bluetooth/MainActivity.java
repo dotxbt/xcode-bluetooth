da6324ac296f5c53fd7e098a4cfca8d6071aec07
Sabituddin Bigbang <sabituddin@inception.asia>
2020-09-16 13:39:29 +0700

    release v0.0.3 AndroidX version

--- app/src/main/java/com/xbteam/xcode_bluetooth/MainActivity.java
+++ app/src/main/java/com/xbteam/xcode_bluetooth/MainActivity.java
@@ -1,6 +1,6 @@
 package com.xbteam.xcode_bluetooth;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
